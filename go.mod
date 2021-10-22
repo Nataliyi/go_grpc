ce69e4d82f5e7629606d3ea0cccb5503f3734600
Natalia.Petrushenkova <natalia.petrushenkova@regium.com>
2021-10-22 11:34:02 +0200

    init

---
+++ go.mod
@@ -0,0 +1,14 @@
+module go_grpc
+
+go 1.13
+
+require (
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.6.0 // indirect
+	golang.org/x/net v0.0.0-20211020060615-d418f374d309 // indirect
+	golang.org/x/sys v0.0.0-20211020174200-9d6173849985 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20211020151524-b7c3a969101a // indirect
+	google.golang.org/grpc v1.41.0
+	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
+	google.golang.org/grpc/examples v0.0.0-20211021223902-4f21cde702d9 // indirect
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"io"
+	"log"
+	"math/rand"
+	"net"
+	"os"
+
+	"google.golang.org/grpc"
+
+	pb "./protos"
+)
+
+var (
+	tls      = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	certFile = flag.String("cert_file", "", "The TLS cert file")
+	keyFile  = flag.String("key_file", "", "The TLS key file")
+	port     = flag.Int("port", os.Getenv("PORT"), "The server port")
+)
+
+type ClusrerizationApiServer struct {
+	pb.UnimplementedClusterizationAPIServer
+}
+
+func (s *ClusrerizationApiServer) UnaryClasterization(ctx context.Context, req *pb.GRPCRequest) (*pb.GRPCResponse, error) {
+	return &pb.GRPCResponse{
+		Pid:     req.GetPid(),
+		Sid:     req.GetSid(),
+		Cluster: rand.Intn(100),
+	}, nil
+}
+
+func (s *ClusrerizationApiServer) StreamClasterization(stream pb.ClusterizationAPI_StreamClasterizationServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+		res := &pb.GRPCResponse{
+			Pid:     req.GetPid(),
+			Sid:     req.GetSid(),
+			Cluster: rand.Intn(100),
+		}
+		err = stream.Send(res)
+	}
+	return nil
+}
+
+func newServer() *ClusrerizationApiServer {
+	return &ClusrerizationApiServer{}
+}
+
+func main() {
+	flag.Parse()
+
+	log.Printf("server: starting on port %s", *port)
+	lis, err := net.Listen("tcp", ":"+*port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	var opts []grpc.ServerOption
+
+	if *tls {
+		if *certFile == "" {
+			*certFile = data.Path("x509/server_cert.pem")
+		}
+		if *keyFile == "" {
+			*keyFile = data.Path("x509/server_key.pem")
+		}
+		creds, err := credentials.NewServerTLSFromFile(*certFile, *keyFile)
+		if err != nil {
+			log.Fatalf("Failed to generate credentials %v", err)
+		}
+		opts = []grpc.ServerOption{grpc.Creds(creds)}
+	}
+
+	grpcServer := grpc.NewServer(opts...)
+	pb.RegisterClusterizationAPIServer(grpcServer, newServer())
+	grpcServer.Serve(lis)
+}
